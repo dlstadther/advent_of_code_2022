15337a31ec5c3ca0863513186c1bbaa06e446f9b
Dillon Stadther <dlstadther@gmail.com>
2022-12-03 00:02:22 -0500

    add template file for reading input files

---
+++ solutions/00.py
@@ -0,0 +1,10 @@
+# from pathlib import Path
+#
+# filepath = Path(__file__).resolve()
+# filename_no_ext = filepath.name.split('.')[0]
+# filedir = filepath.parent
+# input_file = filedir / f"../inputs/{filename_no_ext}.txt"
+#
+# if __name__ == "__main__":
+#     with open(input_file, "r") as infile:
+#         pass

d468a2856a71df56570f20246df55257b53e4abb
Dillon Stadther <dlstadther@gmail.com>
2022-12-04 20:55:40 -0500

    refactor day 1 for new template

--- solutions/01.py
+++ solutions/01.py
@@ -1,24 +1,39 @@
 from pathlib import Path
+from typing import List
 
-filepath = Path(__file__).resolve()
-filename_no_ext = filepath.name.split('.')[0]
-filedir = filepath.parent
-input_file = filedir / f"../inputs/{filename_no_ext}.txt"
+
+def read_input() -> List[str]:
+    filepath = Path(__file__).resolve()
+    filename_no_ext = filepath.name.split('.')[0]
+    filedir = filepath.parent
+    input_file = filedir / f"../inputs/{filename_no_ext}.txt"
+    with open(input_file, "r") as infile:
+        input = infile.readlines()
+    return [line.strip() for line in input]
+
+
+def get_aggregate_calories_desc(input: List[str]) -> List[int]:
+    calories = list()
+    calorie_counter = 0
+    for line in input:
+        if not line:
+            calories.append(calorie_counter)
+            calorie_counter = 0
+            continue
+        calorie_counter += int(line)
+    calories_desc = sorted(calories, reverse=True)
+    return calories_desc
+
+
+def run_part_1(input: List[str]) -> int:
+    return get_aggregate_calories_desc(input)[0]
+
+
+def run_part_2(input: List[str]) -> int:
+    return sum(get_aggregate_calories_desc(input)[0:3])
+
 
 if __name__ == "__main__":
-    map: dict = dict()
-    elf_index = 1
-    with open(input_file, "r") as infile:
-        for line in infile:
-            line = line.strip()
-            if line == "":
-                elf_index += 1
-            else:
-                if map.get(elf_index) is None:
-                    map[elf_index] = 0
-                map[elf_index] += int(line)
-    map_sorted = [v for k, v in sorted(map.items(), key=lambda item: item[1], reverse=True)]
-    single_most = map_sorted[0]
-    print(single_most)  # 69795 - correct
-    top_most = sum(map_sorted[0:3])
-    print(top_most)  # 208437 - correct
+    input = read_input()
+    print(run_part_1(input))  # 69795 - correct
+    print(run_part_2(input))  # 208437 - correct
